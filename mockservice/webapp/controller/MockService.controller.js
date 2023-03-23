138ede21bd5a645d6f155d2bfb8b27ccfd8486b9
Diavine Garcia <diavine.garcia@accenture.com>
2023-03-23 17:59:29 +0000

    mockservice commit

--- mockservice/webapp/controller/MockService.controller.js
+++ mockservice/webapp/controller/MockService.controller.js
@@ -10,6 +10,35 @@ sap.ui.define([
         return Controller.extend("sapips.training.mockservice.controller.MockService", {
             onInit: function () {
 
-            }
-        });
+            },
+
+            onSelectProduct: function(oEvent){
+
+                //Get the Control (List)
+                var oList = oEvent.getSource();
+    
+                //Get the selected item
+                var oSelItem = oList.getSelectedItem();
+    
+                //Get the context binding path
+                var sSelItemPath = oSelItem.getBindingContextPath();
+    
+                //Bind the selected item to the control (SimpleForm in Panel 4)
+                this.getView().byId("idProductDetails").bindElement(
+                    { path: sSelItemPath,
+                      model: "ProductsModel"
+                    })
+                },
+
+                onCreate: function(){
+
+                    var oDataMdl = this.getOwnerComponent().getModel("ProductsModel");
+
+                    var oData = 
+                    { ProductID: 999,
+                      ProductName: "myProduct" }
+
+                    oDataMdl.create("/Products", oData, {})
+                    }
+                });
     });
